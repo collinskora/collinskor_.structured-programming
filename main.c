c0be5f7422f3e1bd1c21626aec37fbcbdd349451
collinskora <mwitakora@gmail.com>
2025-11-26 19:07:38 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int main(){
+
+char name[80];
+
+printf("enter your name:");
+
+scanf("%s",name);
+
+printf("your name is: %s\n", name);
+
+printf("length of name is: %c",name);
+
+
+return 0;
+
+
+
+
+
+}
