a997efa22408922d12541e99d8db52f796944745
collinskora <mwitakora@gmail.com>
2025-11-26 19:06:32 +0300

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main (){
+
+float radius, surfacearea;
+
+printf("enter radius of the sphere:");
+
+ scanf("%f",&radius);
+ surfacearea=4*3.14*radius*radius;
+
+ printf("the surface area of the sphere is:%.2f", surfacearea);
+
+
+
+
+return 0;
+
+
+}
